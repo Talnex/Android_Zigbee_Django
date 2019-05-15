9fcdb2fa4063f2f399ce98ba4c759c8b0ebba18e
Talnex <talnex@icloud.com>
2019-05-15 22:53:56 +0800

    添加了图片

--- mydjango/gallery/models.py
+++ mydjango/gallery/models.py
@@ -4,6 +4,9 @@ from django.db import models
 
 
 class Gallery(models.Model):
-	description = models.CharField(max_length=100)
+    description = models.CharField(default='这是一个空的描述', max_length=100)
+    image = models.ImageField(default='default.png', upload_to='images/')
+    title = models.CharField(default='作品标题', max_length=50)
 
-
+    def __str__(self):
+        return self.title
--- mydjango/mydjango/views.py
+++ mydjango/mydjango/views.py
@@ -3,5 +3,5 @@ from gallery.models import Gallery
 
 
 def home(request):
-	gallerys = Gallery.objects
-	return render(request, 'home.html', {'gallerys':gallerys})
+    gallerys = Gallery.objects
+    return render(request, 'home.html', {'gallerys': gallerys})
