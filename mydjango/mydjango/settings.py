9c28e75b8fbe9b4b8fc54363c0ba6c474a1b9643
Talnex <talnex@icloud.com>
2019-06-26 18:41:22 +0800

    sss

--- mydjango/mydjango/settings.py
+++ mydjango/mydjango/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'mydjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mindbook',
-        'HOST':'',
-        'PORT':'3306',
-        'USER':'root'
-        'PASSWORD':'12345'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
