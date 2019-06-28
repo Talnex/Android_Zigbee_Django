89e39f66e47ae5463d656624fa51db75b79f8efd
Talnex <talnex@icloud.com>
2019-06-28 16:52:51 +0800

    添加了markdown和LaTeX支持

--- mydjango/blog/views.py
+++ mydjango/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from . import models
+import markdown
 # Create your views here.
 
 
@@ -10,4 +11,9 @@ def blog_page(request):
 
 def blog_text(request, blog_id):  # blog_id 作为参数被传递到这个函数里所以要接收
     blog = get_object_or_404(models.Blog, pk=blog_id)
+    blog.text =  markdown.markdown(blog.text,
+                                  extensions=[
+                                     'markdown.extensions.extra',
+                                      'mdx_math',
+                                  ])
     return render(request, 'blog_text.html', {'blog': blog})
